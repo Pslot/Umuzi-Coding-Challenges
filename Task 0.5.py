fff6002d51b24d3b4eb579b64567225c059d67b3
Pslot <pselance@gmail.com>
2021-10-28 12:42:47 +0100

    second commit

--- Task 0.5.py
+++ Task 0.5.py
@@ -7,10 +7,10 @@ side_c = int(input("Enter any number: "))
 
 def triangle_area(side_a, side_b, side_c):
   # To find the Semi-parameter
-  s = (side_a + side_b + side_b) / 2
+  s = (side_a + side_b + side_b) / 2 # formular to calculate the semi-parameter of a triangle
 
   # To find the area
-  area = math.sqrt (s * (s-side_a)*(s-side_b)*(s-side_c))
+  area = math.sqrt (s * (s-side_a)*(s-side_b)*(s-side_c)) #formular to calculating the area of a triangle
   print(area)
 
 triangle_area(side_a, side_b, side_c) 
--- Task 0.7.py
+++ Task 0.7.py
@@ -1,10 +1,10 @@
 #function that converts temperature in Celsius to Fahrenheit
 def c_to_f(C):
-    return ((9/5)*C) + 32
+    return ((9/5)*C) + 32 #formlar to convert Celsius to Fahrenheit
 
 #function that converts temperature in Fahrenheit to Celsius
 def f_to_c(F):
-    return (5/9)*(F-32)
+    return (5/9)*(F-32) #formular to convert Fahrenheit to Celsius
 
 temp = float(input("Enter a temperature "))
 choice = input("Was that temperature in Fahrenheit or Celsius? (c/f?): ")
